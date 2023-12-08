a0404b679479814d3867065bf0b2a2e19b940bb0
crummy <crummynz@gmail.com>
2023-12-08 21:04:47 +1300

    Update eeeeverything. Skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,4 @@
 <script>
-	// Your selected Skeleton theme:
-	import '@skeletonlabs/skeleton/themes/theme-sahara.css';
-	// This contains the bulk of Skeletons required styles:
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-
 	import '../tailwind.css';
 </script>
 
